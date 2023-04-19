bd033055ab19c653aacfadcb1e30137ea77574b1
vityoknumber1 <viktorkysil1997@gmail.com>
2023-04-19 09:22:50 +0300

    After lfake8 checking

--- lesson3/task3.py
+++ lesson3/task3.py
@@ -3,9 +3,10 @@ from pympler import asizeof
 word = input("Type the word for serching: ")
 print(word)
 
+
 def find_in_file(file_name: str, pattern: str):
     with open(file_name, "r", encoding="utf-8") as file:
-        with open("./lesson3/results.txt", "w", encoding="utf-8") as output:    
+        with open("./lesson3/results.txt", "w", encoding="utf-8") as output:
             while True:
                 line = file.readline().replace("\n", "")
                 if not line:
@@ -22,4 +23,3 @@ print(asizeof.asized(list(find_in_file(
     file_name="./lesson3/rockyou.txt",
     pattern=word
 )), detail=1).format())
-
