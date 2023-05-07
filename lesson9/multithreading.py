942448e5f864b4ecb7de9b0baaad4f318741d50f
vityoknumber1 <viktorkysil1997@gmail.com>
2023-05-07 15:02:24 +0300

    Multithreading with encryption and downloading

--- lesson9/multithreading.py
+++ lesson9/multithreading.py
@@ -1,27 +1,51 @@
-# import time
+import os
+import threading
+from multiprocessing import Process
+from time import perf_counter, sleep
+
 import requests
 
 
 # CPU-bound task (heavy computation)
 def encrypt_file(path: str):
-    # print(f"Processing image from {image_url} in process {os.getpid()}")
+    start = perf_counter()
+    image_url = "https://vgorode.ua/img/article/3938/12_main-v1573686449.jpg"
+    print(f"Processing image from {image_url} in process {os.getpid()}")
+    sleep(5)
     # Simulate heavy computation by sleeping for a while
     _ = [i for i in range(100_000_000)]
+    encryption_counter = perf_counter() - start
+    print(f"Time taken for encryption task: {encryption_counter}")
 
 
 # I/O-bound task (downloading image from URL)
 def download_image(image_url):
-    # print(f"Downloading image from {image_url} in
-    # thread {threading.current_thread().name}")
+    start = perf_counter()
+    print(
+        f"Downloading image from {image_url} in"
+        f"thread {threading.current_thread().name}"
+    )
     response = requests.get(image_url)
     with open("image.jpg", "wb") as f:
         f.write(response.content)
+    download_counter = perf_counter() - start
+    print(f"Time for I/O-bound task: {download_counter}")
 
 
-try:
-    encrypt_file("rockyou.txt")
-    download_image("https://picsum.photos/1000/1000")
-    # print(f"Time taken for encryption task: {encryption_counter},
-    # I/O-bound task: {download_counter}, Total: {total} seconds")
-except Exception as e:
-    print(f"Error occurreds: {e}")
+if __name__ == "__main__":
+    start = perf_counter()
+    link = "https://vgorode.ua/img/article/3938/12_main-v1573686449.jpg"
+    try:
+        p1 = Process(target=encrypt_file, args=("path/to/file",))
+        p2 = Process(
+            target=download_image,
+            args=(link,),
+        )
+    except Exception as e:
+        print(f"Error occured: {e}")
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    time_total = perf_counter() - start
+    print(f"Time taken for Total: {time_total} seconds")
